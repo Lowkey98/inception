2eab6aca60d79f86d3d23739f86db71a6936d76b
ayoub <ayoubafdel@gmail.com>
2021-09-29 12:51:53 +0100

    added wp-config.php and conf.d

--- srcs/requirements/mariadb/conf/setup.sh
+++ srcs/requirements/mariadb/conf/setup.sh
@@ -1,7 +1,7 @@
 service mysql start
-#mysql -u root -e "CREATE DATABASE wordpress"
-#mysql -u root -e "CREATE USER '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD'"
-#mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD'"
+mysql -u root -e "CREATE DATABASE wordpress"
+mysql -u root -e "CREATE USER 'ayoub'@'localhost' IDENTIFIED BY 'ayoub'"
+mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'ayoub'@'%' IDENTIFIED BY 'ayoub'"
 #service mysql stop
 ps -e | grep mysqld | awk '{print $1}' > a
 kill -kill `cat a`
