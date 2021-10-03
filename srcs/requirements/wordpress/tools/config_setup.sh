bcbde699062e0a6330b89cfccdd2e757ee3bddab
Ayoub Afdel <ayafdel@s2r5p4.1337.ma>
2021-10-03 13:40:58 +0100

    nginx index

--- srcs/requirements/wordpress/tools/config_setup.sh
+++ srcs/requirements/wordpress/tools/config_setup.sh
@@ -1,5 +1,5 @@
 #config 
-    #echo "listen = 9000" >> /etc/php/7.3/fpm/pool.d/www.conf
+    echo "listen = 9000" >> /etc/php/7.3/fpm/pool.d/www.conf
 #service start
 service php7.3-fpm start
 service php7.3-fpm stop
