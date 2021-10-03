2ef7342aac020fa6f615275c137a71d1a2b8c788
Ayoub Afdel <ayafdel@s2r5p4.1337.ma>
2021-10-03 12:52:46 +0100

    php running in wordpress container

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
 all:
 	docker-compose -f srcs/docker-compose.yml up --build
+wordpress:
+	docker build . -f srcs/requirements/wordpress/Dockerfile
---
+++ srcs/requirements/wordpress/tools/config_setup.sh
@@ -0,0 +1,6 @@
+#config 
+    #echo "listen = 9000" >> /etc/php/7.3/fpm/pool.d/www.conf
+#service start
+service php7.3-fpm start
+service php7.3-fpm stop
+/usr/sbin/php-fpm7.3 --nodaemonize
