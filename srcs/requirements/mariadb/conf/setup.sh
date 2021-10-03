0b4e62cdc105db10720639c5b71841a9056f3bdc
Ayoub Afdel <ayafdel@s2r5p4.1337.ma>
2021-10-03 10:34:21 +0100

    project boilerplate + nginx running with TLS.v2

--- srcs/requirements/mariadb/conf/setup.sh
+++
@@ -1,9 +0,0 @@
-service mysql start
-mysql -u root -e "CREATE DATABASE wordpress"
-mysql -u root -e "CREATE USER 'ayoub'@'localhost' IDENTIFIED BY 'ayoub'"
-mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'ayoub'@'%' IDENTIFIED BY 'ayoub'"
-#service mysql stop
-ps -e | grep mysqld | awk '{print $1}' > a
-kill -kill `cat a`
-sleep 2
-mysqld_safe --user=root
