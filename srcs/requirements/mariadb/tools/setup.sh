cfb8e60ee3bc64e335df521fda9ba6330ca3f08a
Ayoub Afdel <ayafdel@s2r5p4.1337.ma>
2021-10-12 11:37:20 +0100

    import/export wp db

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -4,5 +4,6 @@ service mysql start
     mysql -u root -e "CREATE DATABASE wordpress;"
     mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'ayoub'@'%'; "
     mysql -u root -e "FLUSH PRIVILEGES;"
+    mysql -u ayoub --password=ayoub wordpress < wp.sql 
 service mysql stop
 mysqld_safe
